fecb9294a839f4bfb67d929f0873f775557847e3
Chris Waweru <chriswaweru70@gmail.com>
2019-03-19 13:41:20 +0300

    vue Practise

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Boots',
+        description: 'This is what we love doing since time imemorial doing',
+        image:'./assets/socks-green.png',
+        link: 'https://youtube.com',
+        inventory:3,
+        onSale: true,
+    }
+})
